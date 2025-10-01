ba93c66fa4b899fbb9f43581bdab02f5210b3408
ebrahim pishvaz <e.pishvaz@giloilint.net>
2025-10-01 13:24:38 +0330

    1.2

--- server.py
+++ server.py
@@ -1,23 +1,25 @@
-from fastapi import FastAPI, Request, HTTPException, UploadFile, File
+from fastapi import FastAPI, Request, HTTPException, UploadFile, File, Depends
 from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 import os
-from typing import List
+from typing import List, Annotated
+
 
 root_path = os.getcwd()
-abs_path =  './all_file' 
+abs_path =  './all_file'
 upload_path = os.path.join(root_path,'all_file')
 varlist = ['folder','image','audio','video','pdf','file']
+
+security = HTTPBasic()
 
 def retlist(path=""):
     all_list = []
     for i in range(0,7):
         all_list.append([])
-
-    new_path = os.path.join(abs_path,path)    
+    new_path = os.path.join(abs_path,path)
     files = os.listdir(os.path.join(abs_path,path))
-        
     files.sort()
     for f in files:
         if os.path.isdir(os.path.join(new_path,f)):
@@ -37,6 +39,7 @@ def retlist(path=""):
     return all_list
 
 
+
 # Initialize FastAPI app
 app = FastAPI()
 
@@ -46,27 +49,51 @@ app.mount("/static", StaticFiles(directory=os.path.join(root_path,'static')), na
 # Setup templates (similar to Flask's template_folder)
 templates = Jinja2Templates(directory=os.path.join(root_path,'templates'))
 
-@app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
+@app.get("/")
+async def index(request: Request, credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
     lst = retlist()
+    delitem = 0
+    if credentials.username == 'epfa' and credentials.password == 'e734432e7':
+        delitem=1
     return templates.TemplateResponse("index.html", {
         "request": request,
         "path": "",
         "all_list": lst,
-        "varlist": varlist
+        "varlist": varlist,
+        "delitem": delitem
     })
 
+@app.get("/rm/{code}/{path}")
+async def rm_file(code: str, path: str):
+    otp_chk = pyotp.TOTP('EBRAHIMARSHAGOOGLE')
+    if otp_chk.now() == code:
+        files = glob.glob(f'{abs_path}/{path}')
+        for file in files:
+            os.remove(file)
+    return RedirectResponse(url="/")
+
+@app.get("/del/{path:path}")
+async def del_item(path: str, request: Request, credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
+    if credentials.username == 'epfa' and credentials.password == 'e734432e7':
+        os.remove(f'{abs_path}/{path}')
+    return RedirectResponse(url="/")
+
+
 @app.get("/{path:path}", response_class=HTMLResponse)
-async def dir_listing(path: str, request: Request):
+async def dir_listing(path: str, request: Request, credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
     full_path = os.path.join(abs_path, path)
-    
+    delitem = 0
+    if credentials.username == 'epfa' and credentials.password == 'e734432e7':
+        delitem=1
+
     if os.path.isdir(full_path):
         lst = retlist(path)
         return templates.TemplateResponse("index.html", {
             "request": request,
             "path": path,
             "all_list": lst,
-            "varlist": varlist
+            "varlist": varlist,
+            "delitem": delitem
         })
     else:
         if os.path.exists(full_path):
@@ -74,28 +101,19 @@ async def dir_listing(path: str, request: Request):
         else:
             raise HTTPException(status_code=404, detail="File not found")
 
-'''
+
 # Uncomment if you need this functionality
 import pyotp, glob
 
-@app.get("/rm/{code}/{path:path}")
-async def rm_file(code: str, path: str):
-    otp_chk = pyotp.TOTP('')
-    if otp_chk.now() == code:
-        files = glob.glob(f'{abs_path}/{path}')
-        for file in files:
-            os.remove(file)
-    return RedirectResponse(url="/")
-'''
 @app.post("/uploadfiles")
 async def upload_image(file: List[UploadFile] = File(...)):
-    for fi in file:        
+    for fi in file:
         file_path = os.path.join(abs_path, fi.filename)
         with open(file_path, "wb") as f:
             f.write(await fi.read())
-    return RedirectResponse(url="/", status_code=303)    
+    return RedirectResponse(url="/", status_code=303)
 
 #import uvicorn
 #if __name__ == "__main__":
 #    uvicorn.run(app, host="0.0.0.0", port=8000)
- 
+
